cd0d6c0492dad1f1502501c57070938e194d34d7
Tejas <97tej.warankar@gmail.com>
2018-12-29 14:59:58 +0530

    Updates to NuGEt PAckages
    Updates to NuGEt PAckages

--- API/EIS.WebAPI/Filters/Authorization.cs
+++ API/EIS.WebAPI/Filters/Authorization.cs
@@ -1,26 +1,12 @@
-﻿using EIS.Data.Context;
-using EIS.Repositories.IRepository;
-using EIS.Repositories.Repository;
+﻿using EIS.Repositories.IRepository;
 using EIS.WebAPI.RedisCache;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
-using System.Globalization;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Security.Claims;
-using System.Security.Principal;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace EIS.WebAPI.Filters
 {
