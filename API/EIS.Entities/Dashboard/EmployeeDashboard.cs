3e68cfbdd614145684c4f475aeb30cbd459b0b9c
Uday Soni <45026149+udaysoni123@users.noreply.github.com>
2019-01-22 12:48:42 +0530

    Commit
    Commit

---
+++ API/EIS.Entities/Dashboard/EmployeeDashboard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EIS.Entities.Dashboard
+{
+    public class EmployeeDashboard
+    {
+        public int MonthlyPresentDays { get; set; }
+        public int MonthlyAbsentDays { get; set; }
+        public int TotalLeavesTaken { get; set; }
+        public int TotalLeavesAvailable { get; set; }
+    }
+}
--- WebUI/EISSystem/EISSystem/Controllers/BaseController.cs
+++ WebUI/EISSystem/EISSystem/Controllers/BaseController.cs
@@ -27,34 +27,18 @@ namespace EIS.WebApp.Controllers
         public ArrayList LoadData<T1>(string Url)
         {
             SortEmployee sortEmployee = new SortEmployee();
-            // Skiping number of Rows count
             var start = Request.Form["start"].FirstOrDefault();
-            // Paging Length 10,20
             var length = Request.Form["length"].FirstOrDefault();
-            // Sort Column Name
             sortEmployee.SortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
-            // Sort Column Direction ( asc ,desc)
             sortEmployee.SortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
-            // Search Value from (Search box)
             var search = Request.Form["search[value]"].FirstOrDefault();
-            //Paging Size (10,20,50,100)
             sortEmployee.Skip = start != null ? Convert.ToInt32(start) : 0;
             sortEmployee.PageSize = length != null ? Convert.ToInt32(length) : 0;
-            if(string.IsNullOrEmpty(search))
-            {
-                sortEmployee.Search = null;
-            }
-            else
-            {
-                sortEmployee.Search = search;
-            }
-          
+            sortEmployee.Search=string.IsNullOrEmpty(search)?null:search;          
             HttpClient client = _service.GetService();
             string stringData = JsonConvert.SerializeObject(sortEmployee);
-
             var contentData = new StringContent(stringData, Encoding.UTF8, "application/json");
             HttpResponseMessage response = client.PostAsync(Url, contentData).Result;
-
             ArrayList arrayData = response.Content.ReadAsAsync<ArrayList>().Result;
             return arrayData;
             
