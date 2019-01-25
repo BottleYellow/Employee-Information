4822eb3c6232504c4093ee7062c92a27ab778a29
Tejas <97tej.warankar@gmail.com>
2019-01-25 18:03:44 +0530

    Commit
    Commit

--- WebUI/EISSystem/EISSystem/Controllers/AttendanceController.cs
+++ WebUI/EISSystem/EISSystem/Controllers/AttendanceController.cs
@@ -62,7 +62,6 @@ namespace EIS.WebApp.Controllers
             return Json(new { recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data });
         }    
 
-        [NonAction]
         [HttpPost]
         public IActionResult GetAttendanceSummary(string date, string type)
         {
@@ -137,7 +136,7 @@ namespace EIS.WebApp.Controllers
                 HttpResponseMessage response = client.PostAsJsonAsync("api/attendances/" + id + "", attendance).Result;
                 ViewBag.statusCode = Convert.ToInt32(response.StatusCode);
             }
-            return RedirectToAction("Profile","People",new { PersonId = id});
+            return RedirectToAction("Profile","People",new { PersonId = Cache.GetStringValue("EmployeeCode") });
         }
 
         [HttpPost]
@@ -154,24 +153,24 @@ namespace EIS.WebApp.Controllers
                 Attendance attendances = JsonConvert.DeserializeObject<Attendance>(result);
                 ViewBag.TotalHrs = attendances.TotalHours;
             }
-            return RedirectToAction("Profile", "People", new { PersonId = id });
+            return RedirectToAction("Profile", "People", new { PersonId = Cache.GetStringValue("EmployeeCode") });
         }
         #endregion
 
-        public IActionResult GetEmployeeAttendance()
-        {
-            HttpResponseMessage response = _service.GetResponse("api/Employee");
-            string stringData = response.Content.ReadAsStringAsync().Result;
-            IList<Person> employeesdata = JsonConvert.DeserializeObject<IList<Person>>(stringData);
-            var employees = from e in employeesdata
-                            select new Person
-                            {
-                                Id = e.Id,
-                                FirstName = e.FirstName + " " + e.LastName
-                            };
-            ViewBag.Persons = employees;
-            return View(employees);
-        }
+        //public IActionResult GetEmployeeAttendance()
+        //{
+        //    HttpResponseMessage response = _service.GetResponse("api/Employee");
+        //    string stringData = response.Content.ReadAsStringAsync().Result;
+        //    IList<Person> employeesdata = JsonConvert.DeserializeObject<IList<Person>>(stringData);
+        //    var employees = from e in employeesdata
+        //                    select new Person
+        //                    {
+        //                        Id = e.Id,
+        //                        FirstName = e.FirstName + " " + e.LastName
+        //                    };
+        //    ViewBag.Persons = employees;
+        //    return View(employees);
+        //}
 
 
         #region[Method]
@@ -279,7 +278,7 @@ namespace EIS.WebApp.Controllers
         #endregion
 
 
-        [DisplayName("DateWiseAttendance")]
+        [DisplayName("Attendance Datewise")]
         [HttpGet]
         public IActionResult DateWiseAttendance()
         {
@@ -297,7 +296,7 @@ namespace EIS.WebApp.Controllers
             return View(employees);
         }
         
-        [DisplayName("DateWiseAttendance")]
+        [ActionName("DateWiseAttendance")]
         [HttpPost]
         public IActionResult GetDateWiseAttendance(string fromdate,string todate,string id)
         {
