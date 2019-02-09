f50d588e03924599d4be90ccd7af4ea054a6cdb9
milind09 <40836436+milind09@users.noreply.github.com>
2019-02-09 09:48:12 +0530

    commit
    commit

---
+++ API/EIS.Data/Migrations/20190208091614_sp-GetLocations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EIS.Data.Migrations
+{
+    public partial class spGetLocations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE PROCEDURE [dbo].[GetLocations]
+                    @LocationName varchar(50)
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+                    select * from LMS.tblLocations 
+                END";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- API/EIS.WebAPI/Controllers/Location/LocationController.cs
+++ API/EIS.WebAPI/Controllers/Location/LocationController.cs
@@ -31,6 +31,13 @@ namespace EIS.WebAPI.Controllers.Location
             _repository.Locations.CreateAndSave(location);
             return Ok(location);
         }
+
+        [HttpGet]
+        public IActionResult GetAllLocations()
+        {
+
+            return Ok();
+        }
        
     }
 }
