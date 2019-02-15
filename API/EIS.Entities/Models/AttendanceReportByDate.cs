c11ec9c3f100542655d365225a89712ea0a7a157
Tejas <97tej.warankar@gmail.com>
2019-02-15 11:56:27 +0530

    Commit
    Commit

--- API/EIS.Entities/Models/AttendanceReportByDate.cs
+++ API/EIS.Entities/Models/AttendanceReportByDate.cs
@@ -7,6 +7,8 @@ namespace EIS.Entities.Models
 {
    public class AttendanceReportByDate
     {
+        public string EmployeeCode { get; set; }
+        public string EmployeeName { get; set; }
         public string Date { get; set; }
         public string Status { get; set; }
         public string TimeIn { get; set; }
