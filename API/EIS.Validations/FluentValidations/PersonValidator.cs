c239932f6120cb38ae689ad9ef9fbfc27cd115b3
Uday Soni <45026149+udaysoni123@users.noreply.github.com>
2019-03-30 16:32:07 +0530

    Commit
    Commit

--- API/EIS.Validations/FluentValidations/PersonValidator.cs
+++ API/EIS.Validations/FluentValidations/PersonValidator.cs
@@ -17,7 +17,7 @@ namespace EIS.Validations.FluentValidations
             RuleFor(x => x.FirstName).MaximumLength(50).Matches("^[a-zA-Z ]*$").NotNull();
             RuleFor(x => x.MiddleName).MaximumLength(50).Matches("^[a-zA-Z ]*$");
             RuleFor(x => x.LastName).MaximumLength(50).Matches("^[a-zA-Z ]*$").NotNull();
-            RuleFor(x => x.JoinDate).LessThanOrEqualTo(DateTime.Now.Date).WithMessage("Join Date must be less than or equal to Today's Date").NotNull();
+            RuleFor(x => x.JoinDate).NotNull();
             RuleFor(x => x.Gender).NotNull().WithMessage("Gender must be selected");
             RuleFor(x => x.MobileNumber).Length(10).Matches("^[0-9]*$").NotNull().Must(UniqueMobileNumber).WithMessage("Mobile Number already exists");
             RuleFor(x => x.DateOfBirth).NotNull();
